9df142cb0fd3ef0a512cfaebbdd17d8623dd0b32
hemant-sw <hemant.ks009@gmail.com>
2022-10-08 20:51:09 +0530

    backend

---
+++ discord-backend/controllers/auth/postRegister.js
@@ -0,0 +1,58 @@
+const User = require('../../models/user'); 
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const postRegister= async (req,res) => {
+    try{
+        const {username, mail, password} = req.body; 
+        
+
+        //check if user exists
+        const userExists = await User.exists({ mail: mail.toLowerCase() });
+
+        if (userExists) {
+            return res.status(409).send('E-mail already in in use.');
+        }
+
+        // encrpt password
+        const encryptedPassword = await bcrypt.hash(password,10);
+
+        //create user document and save it to the database
+
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword
+
+        });
+
+        // create JWT token
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                mail
+             },
+             process.env.TOKEN_KEY,
+             {
+                expiresIn: "24h",
+             }
+
+        );
+
+        res.status(201).json({
+            userDetails: {
+                mail: user.mail,
+                token: token,
+                username : user.username,
+
+            }
+        })
+
+    } catch(err) {
+        return res.status(500).send("Error occured.Please try again");
+    }
+  
+};
+
+module.exports = postRegister;
