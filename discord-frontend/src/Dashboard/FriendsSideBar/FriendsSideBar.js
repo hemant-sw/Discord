ec70bfd3e672079959e223d0c450da4e18861fb0
hemant-sw <hemant.ks009@gmail.com>
2022-10-19 00:02:01 +0530

    dashboard UI

---
+++ discord-frontend/src/Dashboard/FriendsSideBar/FriendsSideBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {styled} from '@mui/system';
+
+const MainContainer = styled("div")({
+    width: "224px",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#2F3136",
+  });
+  
+
+export const FriendsSideBar = () => {
+  return (
+    <MainContainer>
+
+    </MainContainer>
+  )
+}
---
+++ discord-frontend/src/Dashboard/Messenger/Messenger.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {styled} from '@mui/system';
+
+const MainContainer = styled('div')({
+    flexGrow: 1,
+    backgroundColor: '#36393f',
+    marginTop:'48px',
+    display:'flex',
+ 
+});
+
+export const Messenger = () => {
+  return (
+    <MainContainer>
+        
+    </MainContainer>
+  )
+}
