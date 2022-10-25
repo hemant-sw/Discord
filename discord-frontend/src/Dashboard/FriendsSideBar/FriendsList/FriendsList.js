b492e29ca73fed1a95186b1325178574ab2611d1
hemant-sw <hemant.ks009@gmail.com>
2022-10-25 23:08:48 +0530

    AddFriendButton funcionality

---
+++ discord-frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {styled} from "@mui/system";
+
+const MainContainer = styled('div')({
+  flexGrow:1,
+  width:"100%",
+});
+
+
+export const FriendsList = () => {
+  return (
+    <MainContainer>
+
+    </MainContainer>
+    
+  );
+}
